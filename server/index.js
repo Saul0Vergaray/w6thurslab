d2003b9010fb3aa65553b7682af73ff7caa44002
Saul Vergaray <saulvergaray99@gmail.com>
2022-04-28 16:49:42 -0600

    ohboy

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const app = express()
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
+const port = process.env.PORT || 4545
 
 app.listen(4545, () => console.log('Take us to warp 4545!'))
 
